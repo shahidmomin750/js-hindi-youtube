5c4592637bd52777088b03797097f2f2db395528
shahidmomin750 <143708047+shahidmomin750@users.noreply.github.com>
2024-08-01 16:34:06 +0000

    datatpes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,20 @@
+console.log("this is the datatypes");
+//use "strict"; treat all js code as newer version 
+//alert(74=+74);  we are using node js not browser in the case of browser this code is carry on 
+console.log(8+8); 
+console.log("shahid");
+ 
+
+// the important datatypes in javascript 
+ 
+// number = 2 to power 53 
+// big int 
+// string = ""
+// null = standalone value 
+// boolean = true or false 
+// undefiende 
+// symbole ="unique"
+
+// object 
+console.log(typeof "hitesh ");
+console.log(typeof null );
