bb72a542d91446d2b81a58ed5fb9dbd92168b789
shahidmomin750 <143708047+shahidmomin750@users.noreply.github.com>
2024-08-01 08:26:55 +0000

    this is variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,6 @@
+console.log("this is the javascript variables");
+const accountId = 112346;
+let accountEmail = "shahidmomin@233gmail.com" ;
+var accountPassword = 23245
+let accountCity = "ranchi";
+console.table([accountId, accountEmail, accountPassword, accountCity]);
