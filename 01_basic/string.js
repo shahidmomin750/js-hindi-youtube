8a97f6ac454de217a3b19abd06c8dd0594118c89
shahidmomin750 <143708047+shahidmomin750@users.noreply.github.com>
2024-08-03 16:13:54 +0000

    string

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -14,6 +14,35 @@ let str2 = " hello"
 let str3 = str1 + str2
 console.log(str3);
 
+// another example of string 
+ 
+console.log("1"+2); //output is same 
+console.log(1+"2"); //output is same 
+console.log(1+2+"3");//output is same 
+console.log("1"+2+3);//output is same 
+
+//Some tricky conversion of string 
+
+let chek = +"";
+let chekbool = chek;
+console.log(chekbool);
+console.log(+"");
+console.log(+true);
+
+//prefix and postfix
+let gameCounter = 1000;
+gameCounter++;//postfix
+//++gameCounter;//prefix
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
 
 
