42d2ab3c8fabf86d545936c96482036303219212
shahidmomin750 <143708047+shahidmomin750@users.noreply.github.com>
2024-08-03 15:12:20 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,21 @@
+console.log("hello world this the lessson of string ");
+
+let value = 20;
+let negValue = -value
+console.log(negValue)
+
+console.log(2-2);
+console.log(2+2);
+console.log(2/2);
+console.log(2*2);
+// lets another example of js 
+let str1 = "shahid"
+let str2 = " hello"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+
+
+
