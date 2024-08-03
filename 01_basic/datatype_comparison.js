501d86d750a84fdb00e38ac19c145fc288532402
shahidmomin750 <143708047+shahidmomin750@users.noreply.github.com>
2024-08-03 19:58:34 +0000

    datatype

---
+++ 01_basic/datatype_comparison.js
@@ -0,0 +1,72 @@
+console.log("hello world this is javascript");
+
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2>=3);
+// console.log(2==3);
+// console.log(2!=3);
+
+
+//now chek for the string test 
+
+// console.log("2">1);
+// console.log(2>"1");
+// console.log(2<"1");
+// console.log(2<"1");
+
+//for cheking of null value 
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null!=0);
+
+//for cheking undefined value 
+
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined==0);
+// console.log(undefined!=0);
+
+
+
+// there are two types of datatypes 
+// 1.primitive datatypes 
+// there are 7 primitive datatypes
+//  number, string, boolean, symbol, undefined, null, Bigint
+
+// 2.reference datatpes (non primitive datatypes)
+// array, object, function
+
+// const score = 100;
+// const scoreValue =100.33;
+// const isLogged = false;
+const outsideTemp = null;
+// let useremail;
+// console.table([score, scoreValue, isLogged, outsideTemp, useremail]);
+
+//let us chek another example of symbol value 
+// let id = Symbol("123")
+// let anotheId = Symbol("123")
+// //console.log(id);
+// console.log(id === anotheId);
+
+const heros = ["shaktimaan", "naagraaj", "doga"];
+const myObj = {
+      name: "shahid",
+      age: 24,
+}
+console.log(myObj);
+
+// function
+const myFunction = function(){
+    console.log("welcome to the function datatype");
+    
+}
+ console.log(typeof heros );
+ 
+
+
+
+
+
